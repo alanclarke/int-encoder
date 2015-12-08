b9635b7929700094659f4ad6ce9fe4a2a77cff94
alanclarke <hi@alz.so>
2015-12-08 01:39:04 +0000

    refactor

--- README.md
+++ README.md
@@ -17,29 +17,23 @@ some bad uses of this utility include:
 Install the module with: `npm install int-encoder`
 
 ```javascript
-var int_encoder = require('int-encoder');
+var encoder = require('int-encoder');
 
-int_encoder.encode(12345678); // "ZXP0"
-int_encoder.decode('ZXP0'); // 12345678
+encoder.encode(12345678); // "ZXP0"
+encoder.decode('ZXP0'); // 12345678
 
 //invoke custom alphabet option
-int_encoder.alphabet('0123456789abcdef') // hex alphabet
+encoder.alphabet('0123456789abcdef') // hex alphabet
 
-int_encoder.encode(255); // "ff"
-int_encoder.decode('ff'); // 255
+encoder.encode(255); // "ff"
+encoder.decode('ff'); // 255
 
 //check what alphabet is being used
-int_encoder.alphabet() // 0123456789abcdef
+encoder.alphabet() // 0123456789abcdef
 
 //convert big hex number using optional base argument
-int_encoder.encode('e6c6b53d3c8160b22dad35a0f705ec09', 16); // 'hbDcW9aE89tzLYjDgyzajJ'
-int_encoder.decode('hbDcW9aE89tzLYjDgyzajJ', 16); // 'e6c6b53d3c8160b22dad35a0f705ec09'
+encoder.encode('e6c6b53d3c8160b22dad35a0f705ec09', 16); // 'hbDcW9aE89tzLYjDgyzajJ'
+encoder.decode('hbDcW9aE89tzLYjDgyzajJ', 16); // 'e6c6b53d3c8160b22dad35a0f705ec09'
 
 ```
-
-## Contributing
-In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/cowboy/grunt).
-
-## License
-Copyright (c) 2012 alan clarke  
-Licensed under the MIT license.
+[![js-standard-style](https://cdn.rawgit.com/feross/standard/master/badge.svg)](https://github.com/feross/standard)
--- test/int-encoder_test.js
+++ test/int-encoder_test.js
@@ -1,72 +1,17 @@
-var int_encoder = require('../lib/int-encoder.js');
-
-/*
-  ======== A Handy Little Nodeunit Reference ========
-  https://github.com/caolan/nodeunit
-
-  Test methods:
-    test.expect(numAssertions)
-    test.done()
-  Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
-*/
-int_encoder.alphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
-
+var intEncoder = require('../lib/int-encoder.js')
+intEncoder.alphabet('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
 exports['encode'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'encoder': function(test) {
-
-    var test_values = [0, 1, 2, 3, 62, 100,1000,354324523453245];
-
-    test_values.forEach(function(i){
-      var encoded = int_encoder.encode( i );
-      test.ok( i.toString().length >= encoded.toString().length, 'encoded string is equal to or longer than int' );
-      test.equal(i,int_encoder.decode(encoded),'decoded string is same as input');
-    });
-
-    var hex = 'e6c6b53d3c8160b22dad35a0f705ec09', encoded_hex = int_encoder.encode(hex,16);
-
-    test.equal(encoded_hex,'hbDcW9aE89tzLYjDgyzajJ', 'should encode large hex values');
-    
-    test.equal(int_encoder.decode(encoded_hex, 16),'e6c6b53d3c8160b22dad35a0f705ec09',  'should decode large hex values');
-    
-    test.done();
-  },
-  'errors':function(test){
-
-    test.throws(function(){
-      int_encoder.encode('not an int');
-    }, Error, 'encode should only accept ints');
-    
-    test.throws(function(){
-      int_encoder.encode(-1);
-    }, Error, 'encode should only accept ints greater or equal to zero');
-
-    test.throws(function(){
-      int_encoder.decode('(*)&^*&(^');
-    }, Error, 'decode should only accept strings made of defined alphabet');
-
-    test.throws(function(){
-      int_encoder.alphabet('aa');
-    }, Error, 'each char in alphabet must be unique');
-
-
-    test.doesNotThrow(function(){
-      int_encoder.alphabet('abcdefghijkl-m');
-      int_encoder.decode('-');
-    }, Error, 'dash character should be allowed if in alphabet')
-    test.done();
+  encoder: function (test) {
+    var hex = 'e6c6b53d3c8160b22dad35a0f705ec09'
+    var values = [0, 1, 2, 3, 62, 100, 1000, 354324523453245]
+    values.forEach(function (i) {
+      var encoded = intEncoder.encode(i)
+      test.ok(String(i).length >= String(encoded).length, 'encoded string is equal to or longer than int')
+      test.equal(i, intEncoder.decode(encoded), 'decoded string is same as input')
+    })
+    var encoded = intEncoder.encode(hex, 16)
+    test.equal(encoded, 'hbDcW9aE89tzLYjDgyzajJ', 'should encode large hex values')
+    test.equal(intEncoder.decode(encoded, 16), 'e6c6b53d3c8160b22dad35a0f705ec09', 'should decode large hex values')
+    test.done()
   }
-};
+}
